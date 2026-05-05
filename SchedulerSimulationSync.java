269abe276a53727718194b7449b985740013a23a
yara205 <445052208@STD.PSAU.EDU.SA>
2026-05-05 14:08:10 +0300

    Add ReentrantLock to protect shared counters (contextSwitchCount, completedProcessCount, totalWaitingTime)

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,9 +5,6 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
-// Added for synchronization
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.Semaphore;
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
